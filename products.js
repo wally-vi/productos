58e8d47d038272e3328bcf2fc49cd8a071d2fd94
unknown <vilacahuaclementefelix@gmail.com>
2021-02-24 10:22:55 -0400

    ProductsManagement added

---
+++ products.js
@@ -0,0 +1,37 @@
+class Products {
+    constructor (name, description, quantity) {
+        this.name = name;
+        this.description;
+        this.quantity = quantity;  
+    }
+}
+class ProductsManagement {
+    constructor(){
+        this.listproducts = []
+    }
+    addProducts(product){
+        this.listproducts.push(product);
+    }
+    removeProducts(product){
+        for (var i = 0; i < this.listproducts.length; i++){
+            if(this.listproducts[i] ==product){
+                this.listproducts.splice(i,1);
+                return;
+            }
+        }
+        return;
+    }
+    updateProducts(product,newproduct){
+        for (var i = 0; i < this.listproducts.length; i++){
+            if(this.listproducts[i] ==product){
+                this.listproducts[i] = newproduct;
+                return;
+            }
+        }
+
+    }
+    showProducts() {
+        return this.Products
+    }
+}
+export {Products, ProductsManagement}
